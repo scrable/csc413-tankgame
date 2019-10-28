08b6cbcf54cfd107219fc720cf0338bce92cc2a6
Corey Russ <cruss.seq11@gmail.com>
2019-10-28 16:27:15 -0700

    add WorldItem class to abstract items in the world that use similar variable and methods

---
+++ src/World/WorldItem.java
@@ -0,0 +1,49 @@
+package World;
+
+public abstract class WorldItem {
+    private int a;
+    private int x;
+    private int y;
+    private int ax;
+    private int ay;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getAx() {
+        return ax;
+    }
+
+    public void setAx(int ax) {
+        this.ax = ax;
+    }
+
+    public int getAy() {
+        return ay;
+    }
+
+    public void setAy(int ay) {
+        this.ay = ay;
+    }
+}
