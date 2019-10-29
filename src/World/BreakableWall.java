2c8ea7f060ec04fc65f631f1c1c9eff7bbba2016
Corey Russ <cruss.seq11@gmail.com>
2019-10-29 13:59:25 -0700

    add drawing walls

--- src/World/BreakableWall.java
+++ src/World/BreakableWall.java
@@ -1,4 +1,15 @@
 package World;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class BreakableWall extends Wall {
+    BreakableWall(String img) throws IOException {
+        super(img);
+    }
+
+    @Override
+    public void drawImage(Graphics g, int x, int y) {
+        g.drawImage(getImg(), x, y, null);
+    }
 }
