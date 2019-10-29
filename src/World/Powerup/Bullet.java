a7f038f6bf493767c7e91a11fcbdbe66905057f0
Corey Russ <cruss.seq11@gmail.com>
2019-10-29 13:05:33 -0700

    add initialization for bullet

--- src/World/Powerup/Bullet.java
+++ src/World/Powerup/Bullet.java
@@ -1,4 +1,14 @@
-package Project.Powerup;
+package World.Powerup;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Bullet {
+    private static Image img;
+
+    public Bullet(String img) throws IOException {
+        Bullet.img = ImageIO.read(new File(img));
+    }
 }
