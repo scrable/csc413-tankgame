92b215bd9bac97a434b5008358112e8b002b3f2b
Corey Russ <cruss.seq11@gmail.com>
2019-10-30 19:15:38 -0700

    extend abstraction to bullet

--- src/World/Powerup/Bullet.java
+++ src/World/Powerup/Bullet.java
@@ -1,14 +1,26 @@
 package World.Powerup;
 
+import World.WorldItem;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class Bullet {
+public class Bullet extends WorldItem {
     private static Image img;
 
     public Bullet(String img) throws IOException {
         Bullet.img = ImageIO.read(new File(img));
+    }
+
+    @Override
+    public void drawImage(Graphics g, int x, int y) {
+
+    }
+
+    @Override
+    public void collisions() {
+
     }
 }
