2e1c85f929a784b834bdf36b29ab4a0b11a3011a
Corey Russ <cruss.seq11@gmail.com>
2019-11-04 10:48:48 -0800

    remove old commented code

--- src/World/Map.java
+++ src/World/Map.java
@@ -7,29 +7,11 @@ import java.io.File;
 import java.io.IOException;
 
 public class Map extends JPanel {
-    private Image image = null;
+    private Image image;
 
     Map(String img) throws IOException {
         image = ImageIO.read(new File(img));
     }
-//    Map() {
-//        MediaTracker mt = new MediaTracker(this);
-//        bg = Toolkit.getDefaultToolkit().getImage("Project/World/Resource/Background.bmp");
-//        mt.addImage(bg, 0);
-//        try {
-//            mt.waitForAll();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//       // g.drawImage(bg, 0, 0, this);
-//        g.drawImage(bg.getScaledInstance(1280, -1, Image. SCALE_SMOOTH), 0, 0, this);
-//
-//    }
 
     @Override
     public Dimension getPreferredSize() {
@@ -37,21 +19,6 @@ public class Map extends JPanel {
     }
 
     void drawImage(Graphics g) {
-//        paintComponent(g);
-        // g.drawImage(image, 0, 0, null);
-
-        //  super.paintComponent(g);
-
-//    if (image != null) {
-//
-//        int imgWidth, imgHeight;
-//
-//
-//        imgWidth = World.getScreenWidth();
-//        imgHeight = World.getScreenHeight();
-//        g.drawImage(image, 0, 0, imgWidth, imgHeight, this);
-//    }
-
         super.paintComponent(g);
         int width = World.SCREEN_WIDTH;
         int height = World.SCREEN_HEIGHT;
